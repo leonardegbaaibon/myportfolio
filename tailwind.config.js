89135feed78a78590a62d700cc5463478dd13139
Leonard Egbaaibon <legbaaibon@gmail.com>
2024-12-18 12:15:25 +0100

    pushing

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,6 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
+export default {
   content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
+    "./src/**/*.{html,js,jsx,ts,tsx}",
   ],
   theme: {
     extend: {},
